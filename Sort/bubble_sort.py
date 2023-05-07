2342bc8828a45ec3bf926ac8e05286c1c5fd7d9f
pok125 <pokljh0411@gmail.com>
2023-05-07 16:35:10 +0900

    Add files via upload

---
+++ Sort/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(l):
+    for i in range(len(l) - 1):
+        for j in range(len(l) - i - 1):
+            if l[j] > l[j + 1] :
+                l[j], l[j + 1] = l[j + 1], l[j]
+    return l
+
+l = [15, 11, 1, 3, 8]
+print(bubble_sort(l))
+
