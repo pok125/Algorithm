3d0e718f0ab9e8f46c559423b0de9e4f2fd88921
kwangho <pokljh0411@gmail.com>
2023-05-13 19:55:48 +0900

    Add files via upload

--- BaekJoon_Problem/그리디/baekjoon_1541.py
+++ BaekJoon_Problem/그리디/baekjoon_1541.py
@@ -1,10 +1,7 @@
 input_string = input()
 l = input_string.split('-')
 for i in range(len(l)):
-    if '+' in l[i]:
-         l[i] = sum(map(int, l[i].split('+')))
-    else:
-         l[i] = int(l[i])
+     l[i] = sum(map(int, l[i].split('+')))
 
 result = l[0]
 for i in range(1, len(l)):
