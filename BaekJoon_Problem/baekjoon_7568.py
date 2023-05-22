fc9e1aa336073d5541304fd4dfc1f7b81e6c5e69
kwangho <pokljh0411@gmail.com>
2023-05-22 22:16:43 +0900

    Delete baekjoon_7568.py

--- BaekJoon_Problem/baekjoon_7568.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-l = [list(map(int, input().split(' '))) for _ in range(n)]
-
-result = ''
-count = 0
-for i in l:
-    count = len(list(filter(lambda x:x[0]>i[0] and x[1]>i[1], l))) + 1
-    result += str(count) + ' '
-print(result.strip())
