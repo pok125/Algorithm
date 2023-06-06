585971aa6dcd67bfac563c1f5e155117ec8d0ea3
kwangho <pokljh0411@gmail.com>
2023-06-06 18:18:09 +0900

    Delete BaekJoon_Problem/브루트포스 directory

--- BaekJoon_Problem/브루트포스/baekjoon_1436.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-count = 1
-result = 666
-while count < n:
-    result += 1
-    if '666' in str(result):
-        count += 1
-
-print(result)
--- BaekJoon_Problem/브루트포스/baekjoon_2309.py
+++
@@ -1,8 +0,0 @@
-import itertools
-
-l = [int(input()) for _ in range(9)]
-ll = list(itertools.combinations(l, 7))
-
-result = list(filter(lambda x:sum(x) == 100, ll))
-for i in sorted(result[0]):
-    print(i)
