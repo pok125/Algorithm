31acb05961ccf2b962044540eb078ebb6c142803
kwangho <pokljh0411@gmail.com>
2023-06-14 21:55:53 +0900

    백준 2178풀이

---
+++ BaekJoon_Problem/그래프/baekjoon_2178.py
@@ -0,0 +1,30 @@
+def bfs():
+    queue = []
+    queue.append((0, 0))
+    visited[0][0] = 1
+
+    pos = [0, 0]
+    while queue:
+        cur_pos = queue.pop(0)
+        
+        for i in check_pos:
+            pos[0] = cur_pos[0] + i[0]
+            pos[1] = cur_pos[1] + i[1]
+
+            if pos[0] >= n or pos[0] < 0 or pos[1] >= m or pos[1] < 0:
+                continue
+            if board[pos[0]][pos[1]] == "0":
+                continue
+            if visited[pos[0]][pos[1]]:
+                continue
+            
+            queue.append((pos[0], pos[1]))
+            visited[pos[0]][pos[1]] = visited[cur_pos[0]][cur_pos[1]] + 1
+        
+check_pos = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+n, m = map(int, input().split(" "))
+board = [input() for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+
+bfs()
+print(visited[n - 1][m - 1])
