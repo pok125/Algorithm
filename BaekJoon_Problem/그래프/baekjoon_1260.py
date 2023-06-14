93fc00c1a747e5d7831a54bf9f29524f1ce04a59
kwangho <pokljh0411@gmail.com>
2023-06-14 21:56:41 +0900

    백준 1260풀이

---
+++ BaekJoon_Problem/그래프/baekjoon_1260.py
@@ -0,0 +1,58 @@
+def bfs(start_node):
+    queue = []
+    queue.append(start_node)
+    ans_bfs.append(start_node)
+
+    visited[start_node] = 1
+
+    while queue:
+        cur_node = queue.pop(0)
+        
+        for node in graph[cur_node]:
+            if not visited[node]:
+                visited[node] = 1
+                ans_bfs.append(node)
+                queue.append(node)
+                
+            
+def dfs(start_node):
+    ans_dfs.append(start_node)
+    visited[start_node]
+
+    for i in graph[start_node]:
+        if not visited[i]:
+            dfs(i)
+    # stack = []
+    # stack.append(start_node)
+
+    # while stack:
+    #     cur_node = stack.pop()
+    #     visited[cur_node] = 1
+    #     ans_dfs.append(cur_node)
+
+    #     for node in graph[cur_node]:
+    #         if not visited[node]:
+    #             stack.append(node)
+    #             break
+                
+n, m, s = map(int, input().split(" "))
+l = [list(map(int, input().split(" "))) for _ in range(m)]
+
+graph = [[] for _ in range(n + 1)]
+visited = [0] * (n + 1)
+ans_dfs = []
+ans_bfs = []
+
+for start, next in l:
+    graph[start].append(next)
+    graph[next].append(start)
+
+for i in range(1, len(graph)):
+    graph[i].sort()
+
+dfs(s)
+for i in range(len(visited)):
+    visited[i] = 0
+bfs(s)
+print(ans_dfs)
+print(ans_bfs)
