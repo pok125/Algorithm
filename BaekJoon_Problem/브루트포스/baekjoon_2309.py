68debddecf2ab0be80f7358b11b15809b727c7db
kwangho <pokljh0411@gmail.com>
2023-06-16 18:51:44 +0900

    백준 2309 재풀이

--- BaekJoon_Problem/브루트포스/baekjoon_2309.py
+++ BaekJoon_Problem/브루트포스/baekjoon_2309.py
@@ -1,8 +1,27 @@
 import itertools
 
 l = [int(input()) for _ in range(9)]
+# 풀이 1
 ll = list(itertools.combinations(l, 7))
 
 result = list(filter(lambda x:sum(x) == 100, ll))
 for i in sorted(result[0]):
     print(i)
+# 풀이 2
+def my_combination(n, m, selected):
+    if n == 9:
+        if len(selected) == m:
+            result.append(selected)
+        return
+    
+    my_combination(n + 1, m, selected + [l[n]])
+    my_combination(n + 1, m, selected)
+
+result = []
+my_combination(0, 7, [])
+for i in result:
+    if sum(i) == 100:
+        answer = sorted(i)
+        break
+
+print(answer)
