a3e992d789ae0ad6dacb1a9f8f91863461807af9
kwangho <pokljh0411@gmail.com>
2023-06-29 13:37:06 +0900

    백준 11724풀이

--- BaekJoon_Problem/그래프/backjoon_11724.py
+++ BaekJoon_Problem/그래프/backjoon_11724.py
@@ -1,12 +1,39 @@
-n, m = map(int, input().split(" "))
+import sys
+
+def bfs(start_node):
+    global answer
+
+    if visited[start_node] == 1:
+        return
+    
+    queue = []
+    queue.append(start_node)
+    visited[start_node] = 1
+    answer += 1
+
+    while queue:
+        cur_node = queue.pop(0)
+
+        for node in range(n):
+            if board[cur_node][node] == 0:
+                continue
+            if visited[node] == 1:
+                continue
+
+            queue.append(node)
+            visited[node] = 1
+
+n, m = map(int, sys.stdin.readline().split(" "))
 board = [[0] * n for _ in range(n)]
 visited = [0] * n
 
-l = [list(map(int, input().split(" "))) for _ in range(m)]
+l = [list(map(int, sys.stdin.readline().split(" "))) for _ in range(m)]
 
 for s, e in l:
     board[s - 1][e - 1] = 1
     board[e - 1][s - 1] = 1
 
-for i in board:
-    print(i)
+answer = 0
+for i in range(n):
+    bfs(i)
+print(answer)
