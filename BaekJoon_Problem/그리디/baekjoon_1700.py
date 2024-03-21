5655830cbaf09d368170c43864a009e71f7e7b67
pok125 <pokljh0411@gmail.com>
2024-03-21 12:45:52 +0900

    백준 1700풀이 수정

--- BaekJoon_Problem/그리디/baekjoon_1700.py
+++ BaekJoon_Problem/그리디/baekjoon_1700.py
@@ -1,33 +1,32 @@
 import sys
 
 N, K = map(int, sys.stdin.readline().split(" "))
-# products = list(map(int, sys.stdin.readline().split(" ")))
-# use_queue = [products[i] for i in range(N)]
-# wait_queue = [products[i] for i in range(N, N + N)]
-# count = 0
+elecs = list(map(int, sys.stdin.readline().split(" ")))
+usings = []
+count = 0
 
-elec_list = list(map(int, input().split()))
-plug = set()
-cnt = 0
+if N >= K:
+    print(count)
+else:
+    for i in range(K):
+        next_elec = elecs[i]
+        if next_elec in usings:
+            continue
+        if len(usings) < N:
+            usings.append(next_elec)
+            continue
+        
+        priority = []
+        temp = elecs[i:]
+        for elec in usings:
+            if elec in temp:
+                priority.append(temp.index(elec))
+            else:
+                priority.append(1000)
+        
+        remove_target = priority.index(max(priority))
+        usings.pop(remove_target)
+        usings.append(next_elec)
+        count += 1
 
-def find_latest(idx) :
-  result = 0
-  max_idx = -1
-  for num in plug :
-    try :
-      num_idx = elec_list[idx:].index(num)
-    except :
-      num_idx = K
-    if max_idx < num_idx :
-      result, max_idx = num, num_idx
-  
-  return result
-  
-for idx, num in enumerate(elec_list) :
-  plug.add(num)
-  if len(plug) > N :
-    cnt += 1
-    latest_used = find_latest(idx)
-    plug.discard(latest_used)
-
-print(cnt)
+    print(count)
