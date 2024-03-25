4f9d4137dbaf5cd385cfdf46b9f5423d84f53964
pok125 <pokljh0411@gmail.com>
2024-03-25 15:46:27 +0900

    백준 2217 다른 풀이

--- BaekJoon_Problem/그리디/baekjoon_2217.py
+++ BaekJoon_Problem/그리디/baekjoon_2217.py
@@ -15,3 +15,16 @@ for i in range(len(keys)):
     count += d[keys[i]]
 
 print(result)
+
+# 다른 풀이
+import sys
+N = int(sys.stdin.readline())
+nums = [int(sys.stdin.readline()) for _ in range(N)]
+
+nums.sort(reverse=True)
+answer = 0
+
+for i, num in enumerate(nums, start=1):
+    answer = max(answer, num * i)
+
+print(answer)
