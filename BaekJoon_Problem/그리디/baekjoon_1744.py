2a5c2731382a46663207c63935358643fde9e86f
pok125 <pokljh0411@gmail.com>
2024-03-26 00:00:27 +0900

    백준 1744다른 코드

--- BaekJoon_Problem/그리디/baekjoon_1744.py
+++ BaekJoon_Problem/그리디/baekjoon_1744.py
@@ -31,3 +31,47 @@ if positive_len % 2 != 0:
 result = sum(l) if l else 0
 result += 1 * one_count
 print(result)
+
+# 다른 코드
+import sys
+N = int(sys.stdin.readline())
+minus = []
+plus = []
+flag_zero = False
+hap_one = 0
+for _ in range(N):
+    num = int(sys.stdin.readline())
+    if num < 0:
+        minus.append(num)
+    else:
+        if num == 0:
+            flag_zero = True
+            continue
+        elif num == 1:
+            hap_one += 1
+            continue
+
+        plus.append(num)
+
+minus.sort()
+plus.sort()
+answer = 0
+if len(minus) % 2 == 0:
+    answer += sum(map(lambda x:x[0]*x[1], zip(minus[::2], minus[1::2])))
+else:
+    temp = minus.pop()
+    if flag_zero:
+        temp = 0
+    answer += temp
+    answer += sum(map(lambda x:x[0]*x[1], zip(minus[::2], minus[1::2])))
+
+if len(plus) % 2 == 0:
+    answer += sum(map(lambda x:x[0]*x[1], zip(plus[::2], plus[1::2])))
+else:
+    temp = plus.pop(0)
+    answer += temp
+    answer += sum(map(lambda x:x[0]*x[1], zip(plus[::2], plus[1::2])))
+
+if hap_one:
+    answer += hap_one
+print(answer)
