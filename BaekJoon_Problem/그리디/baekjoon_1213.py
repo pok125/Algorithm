e4e401281405b148c345b491e780e57cd5ecd94c
pok125 <pokljh0411@gmail.com>
2024-03-26 12:15:56 +0900

    백준 1213다른 코드

--- BaekJoon_Problem/그리디/baekjoon_1213.py
+++ BaekJoon_Problem/그리디/baekjoon_1213.py
@@ -15,3 +15,30 @@ else:
         s += i * (c[i] // 2)
     result = s + mid_char + s[::-1]
     print(result)
+
+# 다른 코드
+import sys
+from collections import Counter
+
+string = sys.stdin.readline().rstrip()
+c = Counter(string)
+sorted_c = sorted(c.items(), key=lambda x:x[0])
+temp = []
+count = 0
+mid_char = ""
+answer = ""
+flag = False
+for key, value in sorted_c:
+    if value % 2 != 0:
+        count += 1
+        mid_char = key
+
+    if count > 1:
+        flag = True
+        break
+    
+    temp.extend(key * (value // 2))
+
+answer = ''.join(temp) + mid_char + ''.join(temp[::-1])
+answer = answer if flag is False else "I'm Sorry Hansoo"
+print(answer)    
