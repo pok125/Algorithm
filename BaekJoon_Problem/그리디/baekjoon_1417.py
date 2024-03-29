28a88a6b0411f2d362c98c0d8db6f7a62dc2be3d
pok125 <pokljh0411@gmail.com>
2024-03-29 22:43:35 +0900

    백준 1417다른 풀이

--- BaekJoon_Problem/그리디/baekjoon_1417.py
+++ BaekJoon_Problem/그리디/baekjoon_1417.py
@@ -1,20 +1,37 @@
-import sys
+# import sys
+
+# N = int(sys.stdin.readline())
+# l = [int(sys.stdin.readline()) for _ in (range(N))]
+
+# dasom = l.pop(0)
+# l.sort(reverse=True)
+
+# count = 0
+# max_num = l[0] if l else 0
+# while l and dasom <= max_num:
+#     dasom += 1
+#     l[0] -= 1
+#     count += 1
+    
+#     max_num = max(l)
+#     if l[0] < max_num:
+#         l.sort(reverse=True)
+
+# print(count)
+
+# 우선순위 큐 풀이
+import sys, heapq
 
 N = int(sys.stdin.readline())
-l = [int(sys.stdin.readline()) for _ in (range(N))]
+l = [int(sys.stdin.readline()) * -1 for _ in (range(N))]
 
-dasom = l.pop(0)
-l.sort(reverse=True)
-
+dasom = l.pop(0) * -1
+heapq.heapify(l)
 count = 0
-max_num = l[0] if l else 0
-while l and dasom <= max_num:
+
+while l and dasom <= (l[0] * -1):
+    heapq.heappushpop(l, l[0] + 1)
     dasom += 1
-    l[0] -= 1
     count += 1
-    
-    max_num = max(l)
-    if l[0] < max_num:
-        l.sort(reverse=True)
 
 print(count)
